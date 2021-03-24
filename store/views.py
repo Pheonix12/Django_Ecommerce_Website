2baa829fd4a262084705b5afcfbddd9f26785f95
Shahriar <s.shaown@outlook.com>
2021-03-24 19:20:29 +0600

    added add/update cart event handlers and created csrf token.

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 # Create your views here.
 
 def store(request):
@@ -33,3 +34,7 @@ def checkout(request):
 
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe = False)
